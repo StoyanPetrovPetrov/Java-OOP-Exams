9a56ab68f5623be245c8f50de21be10723d4122f
StoyanPetrovPetrov <104095552+StoyanPetrovPetrov@users.noreply.github.com>
2022-08-02 17:24:40 +0300

    Add files via upload

---
+++ christmasRaces/entities/cars/MuscleCar.java
@@ -0,0 +1,7 @@
+package christmasRaces.entities.cars;
+
+public class MuscleCar extends BaseCar{
+    public MuscleCar(String model, int horsePower) {
+        super(model, horsePower, 5000);
+    }
+}
---
+++ christmasRaces/entities/cars/SportsCar.java
@@ -0,0 +1,7 @@
+package christmasRaces.entities.cars;
+
+public class SportsCar extends BaseCar{
+    public SportsCar(String model, int horsePower) {
+        super(model, horsePower, 3000);
+    }
+}
