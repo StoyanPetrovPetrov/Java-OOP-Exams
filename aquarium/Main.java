e34edc47bf9a235fe9557e19e52276f81547222c
StoyanPetrovPetrov <104095552+StoyanPetrovPetrov@users.noreply.github.com>
2022-08-10 18:26:49 +0300

    Add files via upload

--- aquarium/Main.java
+++ aquarium/Main.java
@@ -1,12 +1,16 @@
 package aquarium;
 
-import aquarium.core.Engine;
-import aquarium.core.EngineImpl;
-
-
 public class Main {
     public static void main(String[] args) {
-        Engine engine = new EngineImpl();
-        engine.run();
+        Fish f = new Fish("name");
+        Fish f1 = new Fish("name1");
+        Fish f2 = new Fish("name2");
+        Aquarium aquarium = new Aquarium("aqua", 10);
+
+        aquarium.add(f);
+        aquarium.add(f1);
+        aquarium.add(f2);
+        aquarium.remove("name2");
+        System.out.println(aquarium.report());
     }
 }
